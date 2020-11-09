cd9201b60084b2efa6844d36beba5c55b0681747
Guillermo Croppi <guillermocroppi@gmail.com>
2020-11-09 18:17:46 -0300

    Added docker ignore and config update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'somosmuchas-web-nuxt',
+    title: 'Somos Muchas',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
