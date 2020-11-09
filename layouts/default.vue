46b2efd32a3b83947c49c91f3d614ec3328220b7
Guillermo Croppi <guillermocroppi@gmail.com>
2020-11-09 17:04:19 -0300

    Eslint updates

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,11 +5,5 @@
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      
-    }
-  }
-}
+export default {}
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,6 @@
 
 <script>
 export default {
-  name: 'HomePage',  
+  name: 'HomePage'
 }
 </script>
