1ca1baa0f8030a97801ef2fbe5d9e0f8d4563899
DevHormiga <devhormiga@gmail.com>
2021-10-30 19:21:47 -0300

    maquetado seccion agenda

--- pages/agendas.vue
+++ pages/agendas.vue
@@ -20,54 +20,72 @@
       <div class="column is-3 has-text-centered">
         <img src="~/assets/arrowDown.svg" class="image an-icon" alt="">
         <h2 class="is-size-4">
-            Descarga
+          Descargar
         </h2>
         <br>
-        <p class="is-size-4">
-            Aqui podras descargar nuestra plantilla para co-creacion de agendas        </p>
+        <div>
+          <p class="is-size-4">
+            Aqui podras descargar nuestra plantilla para co-creacion de agendas
+          </p>
+        </div>
         <br>
-        <a href="https://actua.somosmuchas.org/" class="button is-primary is-rounded has-text-weight-bold is-medium">Quiero actuar</a>
-
+        <br>
+        <a href="https://actua.somosmuchas.org/" class="mt-2 button is-primary is-rounded has-text-weight-bold is-medium">Quiero actuar</a>
       </div>
       <div class="column is-3 is-offset-1 has-text-centered">
         <img src="~/assets/share.svg" class="image an-icon" alt="">
         <h2 class="is-size-4">
-            Comparte
+          Comparte
         </h2>
         <br>
         <p class="is-size-4">
-            Sube tu agenda, para que podamos compartirla con la comunnidad y abrir el debate y la difusion        </p>
+          Sube tu agenda, para que podamos compartirla con la comunnidad y abrir el debate y la difusion
+        </p>
         <br>
         <a href="https://actua.somosmuchas.org/" class="button is-primary is-rounded has-text-weight-bold is-medium">Quiero actuar</a>
-
       </div>
     </div>
     <br>
     <!-- <img src="~/assets/title02.svg" class="image mb-6 image-title" alt=""> -->
     <div>
-        <h5 data-v-2a183b29="" class="title mt-5 pt-5 is-2 has-text-lefted has-text-primary has-text-weight-bold">
+      <h5 data-v-2a183b29="" class="title mt-5 pt-5 is-2 has-text-lefted has-text-primary has-text-weight-bold">
         Ultimos subidos<br data-v-2a183b29="">
       </h5>
     </div>
     <div class="columns">
       <div class="column">
-        <!-- <div class="actuar-agenda-box">
+        <div class="actuar-agenda-box">
           <img src="~/assets/paridad-logo.svg" alt="">
-        </div> -->
-        <casos-box class="actuar-agenda-box" title="AGENDA" description="Campaña nacional que promueve y facilita la participación de las mujeres en política. Por lo justo: cincuenta / sí cuenta." href="https://actua.somosmuchas.org/proyecto/paridadya#activa" label='ver'>
+          <a href="#" target="_blank" class="button is-primary has-text-weight-bold is-rounded enter-button is-medium">
+            ver
+          </a>
+        </div>
+      <!-- <casos-box  title="AGENDA" description="Campaña nacional que promueve y facilita la participación de las mujeres en política. Por lo justo: cincuenta / sí cuenta." href="https://actua.somosmuchas.org/proyecto/paridadya#activa" label='ver'>
              <img src="~/assets/paridad-logo.svg" alt="">
-        </casos-box>
+        </casos-box> -->
       </div>
       <div class="column">
         <div class="actuar-agenda-box">
           <img src="~/assets/logo-sin-mujeres.png" alt="">
+          <a href="#" target="_blank" class="button is-primary has-text-weight-bold is-rounded enter-button is-medium">
+            ver
+          </a>
         </div>
       </div>
       <div class="column">
         <div class="actuar-agenda-box">
           <img src="~/assets/logo-recicladores.png" alt="">
+          <a href="#" target="_blank" class="button is-primary has-text-weight-bold is-rounded enter-button is-medium">
+            ver
+          </a>
         </div>
       </div>
+    </div>
+    <div class="container">
+      <div data-mc-src="4ef2a77e-3497-45e3-bf68-7f83f306f342#instagram">
+      </div>
+      <script src="https://cdn2.woxo.tech/a.js#617dc3d6cd3af80016260b49" async data-usrc>
+      </script>
     </div>
   </div>
 </template>
@@ -109,5 +127,17 @@ export default {
 .an-icon{
     width:257px;
     margin: 0 auto 20px;
+}
+.enter-button{
+  position: absolute;
+  bottom: -20px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  z-index: 150;
+  &:hover{
+    color: $primary;
+    background-color: white;
+    border: 2px solid $primary;
+  }
 }
 </style>
